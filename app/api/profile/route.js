64160bdbac5e0f37dfe056f2634d895435a290bf
faysal <devfaysalbd@gmail.com>
2024-07-31 08:15:02 +0600

    learm cookies and chacing nad header

--- app/api/profile/route.js
+++ app/api/profile/route.js
@@ -1,8 +1,16 @@
-import { headers } from "next/headers";
+import { cookies, headers } from "next/headers";
 
 export async function GET( request ) {
      const headerList = headers();
      console.log(headerList.get("Authorization"));
+     
+     cookies().set("page", "3")
+     console.log(request .cookies.get("theme"));
+     console.log(cookies().get("page"));
 
-     return new Response("profile api")
+     return new Response("profile api", {
+        headers: {
+            "Set-Cookie": "theme=dark"
+        }
+     })
 }
